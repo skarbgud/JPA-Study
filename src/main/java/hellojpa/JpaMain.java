61cef2dc739af624c816a95a02a44fcba76b7854
GyuHyeong Nam <sjarbgud@gmail.com>
2022-05-05 22:14:25 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,92 +16,108 @@ public class JpaMain {
         tx.begin();
 
         try {
-            /* Create
+//
+//            /* Create
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//             */
+//
+//            Member findMember = em.find(Member.class, 1L);
+//
+//            /* Read
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//             */
+//
+//            /* Delete
+//            em.remove(findMember);
+//             */
+//
+//            /* Update
+//            findMember.setName("HelloJPA");
+//             */
+//
+//            // Member 테이블이 아니라 해당 엔티티 객체를 다 불러온다!!
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1) // pagination -> 방언에 맞춰서 각 DB 벤더사에 맞게 SQL을 맞춰줌
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
+//
+//            /*
+//               JPAQL은 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
+//               SQL을 추상화해서 특정 데이터베이스 SQL에 의존하지 않는다.
+//               JPQL을 한마디로 정리하면 객체지향 SQL
+//             */
+//
+//            // 비영속 상태
+//           /* Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            // 영속 상태
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member); // JPA 1차 캐시에 저장
+////            em.detach(member); // 영속성 컨텍스트에서 분리, 준영속 상태
+//            System.out.println("=== AFTER ===");*/
+//
+//            Member findDbFindMember = em.find(Member.class, 101L);
+//            System.out.println("findDbFindMember.id = " + findDbFindMember.getId());
+//            System.out.println("findDbFindMember.name = " + findDbFindMember.getName());
+//            Member primaryCacheFindMember = em.find(Member.class, 101L);
+//            System.out.println("primaryCacheFindMember.id = " + primaryCacheFindMember.getId());
+//            System.out.println("primaryCacheFindMember.name = " + primaryCacheFindMember.getName());
+//
+//            System.out.println("result = " + (findDbFindMember == primaryCacheFindMember)); // 영속 엔티티의 동일성을 보장 해 준다.
+//
+//
+////            Member member1 = new Member(150L, "A");
+////            Member member2 = new Member(160L, "B");
+//            Member member = em.find(Member.class, 150L); // 영속 엔티티를 조회하고
+//            // ----- member는 영속 상태 -----------
+//            member.setName("AAAA"); // 값을 변경하기만 하면 변경감지(Dirty Checking)로 자동으로 변경해준다.
+//
+//            // 영속성 컨텍스트에서 분리
+////            em.detach(member);
+//            em.clear(); // 영속성 컨텍스트를 그냥 통째로 초기화
+////            em.close(); // 영속성 컨텍스트를 종료
+//            // 다시 영속성 컨텍스트에 올려서 조회하게 된다.
+//            Member member2 = em.find(Member.class, 150L); // 영속 엔티티를 다시 조회하게 된다
+//
+////            em.persist(member1);
+////            em.persist(member2);
+////            em.remove(member); 삭제
+//
+////            Member member = new Member(200L, "member200");
+////            em.persist(member);
+//
+//            /* 영속성 컨텍스트를 플러시하는 방법
+//             1. em.flush() - 직접 호출
+//             2. 트랜잭션 커밋 - 플러시 자동 호출
+//             3. JPQL 쿼리 실행 - 플러시 자동 호출
+//             */
+////            em.flush(); // 이 시점에 DB에 쿼리를 날린다. => 영속성 컨텍스트에 있는 쓰기 지연 SQL 저장소에 있는것들이 DB에 반영이 된다.(영속성 컨텍스트의 변경 내용을 데이터베이스에 동기화한다) => 실질적인 반영은 커밋에서!
+//
+//            System.out.println("=====================");
+
             Member member = new Member();
+            member.setId(1L);
+            member.setUsername("A");
+            member.setRoleType(RoleType.USER);
+
             member.setId(2L);
-            member.setName("HelloB");
-             */
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
 
-            Member findMember = em.find(Member.class, 1L);
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-            /* Read
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
-             */
-
-            /* Delete
-            em.remove(findMember);
-             */
-
-            /* Update
-            findMember.setName("HelloJPA");
-             */
-
-            // Member 테이블이 아니라 해당 엔티티 객체를 다 불러온다!!
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1) // pagination -> 방언에 맞춰서 각 DB 벤더사에 맞게 SQL을 맞춰줌
-                    .setMaxResults(10)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
-
-            /*
-               JPAQL은 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
-               SQL을 추상화해서 특정 데이터베이스 SQL에 의존하지 않는다.
-               JPQL을 한마디로 정리하면 객체지향 SQL
-             */
-
-            // 비영속 상태
-           /* Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            // 영속 상태
-            System.out.println("=== BEFORE ===");
-            em.persist(member); // JPA 1차 캐시에 저장
-//            em.detach(member); // 영속성 컨텍스트에서 분리, 준영속 상태
-            System.out.println("=== AFTER ===");*/
-
-            Member findDbFindMember = em.find(Member.class, 101L);
-            System.out.println("findDbFindMember.id = " + findDbFindMember.getId());
-            System.out.println("findDbFindMember.name = " + findDbFindMember.getName());
-            Member primaryCacheFindMember = em.find(Member.class, 101L);
-            System.out.println("primaryCacheFindMember.id = " + primaryCacheFindMember.getId());
-            System.out.println("primaryCacheFindMember.name = " + primaryCacheFindMember.getName());
-
-            System.out.println("result = " + (findDbFindMember == primaryCacheFindMember)); // 영속 엔티티의 동일성을 보장 해 준다.
-
-
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-            Member member = em.find(Member.class, 150L); // 영속 엔티티를 조회하고
-            // ----- member는 영속 상태 -----------
-            member.setName("AAAA"); // 값을 변경하기만 하면 변경감지(Dirty Checking)로 자동으로 변경해준다.
-
-            // 영속성 컨텍스트에서 분리
-//            em.detach(member);
-            em.clear(); // 영속성 컨텍스트를 그냥 통째로 초기화
-//            em.close(); // 영속성 컨텍스트를 종료
-            // 다시 영속성 컨텍스트에 올려서 조회하게 된다.
-            Member member2 = em.find(Member.class, 150L); // 영속 엔티티를 다시 조회하게 된다
-
-//            em.persist(member1);
-//            em.persist(member2);
-//            em.remove(member); 삭제
-
-//            Member member = new Member(200L, "member200");
-//            em.persist(member);
-
-            /* 영속성 컨텍스트를 플러시하는 방법
-             1. em.flush() - 직접 호출
-             2. 트랜잭션 커밋 - 플러시 자동 호출
-             3. JPQL 쿼리 실행 - 플러시 자동 호출
-             */
-//            em.flush(); // 이 시점에 DB에 쿼리를 날린다. => 영속성 컨텍스트에 있는 쓰기 지연 SQL 저장소에 있는것들이 DB에 반영이 된다.(영속성 컨텍스트의 변경 내용을 데이터베이스에 동기화한다) => 실질적인 반영은 커밋에서!
-
-            System.out.println("=====================");
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,26 +1,40 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
-@Table(name = "MBR")
 public class Member {
 
     @Id
     private Long id;
-    @Column(unique = true, length = 10) // DDL을 생성할 때만 사용되고 JPA 실행 로직에는 영향을 주지 않는다.
-    private String name;
-    private int age;
+
+    @Column(name = "name", nullable = false, length = 10, columnDefinition = "varchar(100) default 'EMPTY'") // 컬럼
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) // enum 타입 매핑 (기본이 ORDINAL) => 순서대로 생성하게 되면 나중에 추가시에 겹치게 된다. [ORDINAL 사용 X]
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP) // 날짜 타입 매핑
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob // BLOB, CLOB 매핑
+    private String description;
+
+    @Transient // 컬럼 생성 안됨
+    private int temp;
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -31,11 +45,60 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
 }
