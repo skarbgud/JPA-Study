9593893faef94fdfe2510eb68d2ff557a05e784b
GyuHyeong Nam <sjarbgud@gmail.com>
2022-05-28 01:26:22 +0900

    값 타입의 비교

--- jpashop/src/main/java/jpabook/jpashop/domain/ValueMain.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/ValueMain.java
@@ -4,13 +4,23 @@ public class ValueMain {
 
     public static void main(String[] args) {
         // 자바의 기본 타입은 절대 공유 X
+//        int a = 10;
+//        int b = a; // 값이 복사
+//
+//        b = 20;
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+
         int a = 10;
-        int b = a; // 값이 복사
+        int b = 10;
 
-        b = 20;
+        System.out.println("a==b : " + (a == b));
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
 
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1 equals address2 : " + (address1.equals(address2)));
     }
 }
