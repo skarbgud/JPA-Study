3e193b2565de2cb870c71d5e9ae0584f41872e36
GyuHyeong Nam <sjarbgud@gmail.com>
2022-07-24 17:18:43 +0900

    조건식(CASE, COALESCE, NULLIF)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -106,7 +106,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
@@ -142,16 +142,35 @@ public class JpaMain {
 
 //            String query = "select m.username, 'HELLo', true FROM Member m " +
 //                    "where m.type = jpql.MemberType.ADMIN";
-            String query = "select m.username, 'HELLo', true FROM Member m " +
-                    "where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
+//            String query = "select m.username, 'HELLo', true FROM Member m " +
+//                    "where m.type = :userType";
+//            List<Object[]> result = em.createQuery(query)
+//                    .setParameter("userType", MemberType.ADMIN)
+//                    .getResultList();
+//
+//            for (Object[] objects : result) {
+//                System.out.println("objects[0] = " + objects[0]);
+//                System.out.println("objects[1] = " + objects[1]);
+//                System.out.println("objects[2] = " + objects[2]);
+//            }
 
-            for (Object[] objects : result) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
+            String caseQuery =
+                    "select " +
+                            "case when m.age <= 10 then '학생요금' " +
+                            "     when m.age >=60  then '경로요금' " +
+                            "     else '일반요금' " +
+                            "end " +
+                    "from Member m";
+            String coalesceQuery = "select coalesce(m.username, '이름 없는 회원')" +
+                    " as username from Member m";
+            String nullifQuery = "select nullif(m.username, '관리자') as username " +
+                    "from Member m";
+
+            List<String> result = em.createQuery (nullifQuery , String.class)
+                            .getResultList();
+
+            for (String s : result) {
+                System.out.println("s = " + s);
             }
 
             tx.commit();
