a5e8d2ec8bad9cf0e620a90d0ce9d0a47ef53bae
GyuHyeong Nam <sjarbgud@gmail.com>
2022-09-18 00:47:37 +0900

    페치 조인 1 - 기본

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -193,18 +193,28 @@ public class JpaMain {
 //                System.out.println("s = " + s);
 //            }
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
@@ -215,7 +225,15 @@ public class JpaMain {
             // 묵시적 내부 조인 쿼리
 //            String query = "select t.members From Team t";
             // 명시적 조인 사용 해야함 -> FROM 절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색 가능
-            String query = "select m.username From Team t join t.members m";
+//            String query = "select m.username From Team t join t.members m";
+
+//            String query = "select m From Member m";
+
+            // 페치 조인으로 가져오는 쿼리
+//            String query = "select m From Member m join fetch m.team";
+
+            // 컬렉션 페치 조인
+            String query = "select t From Team t join fetch t.members";
 
 //            String query = "select t.members.size From Team t";
             
@@ -223,15 +241,31 @@ public class JpaMain {
 //                            .getResultList();
 //            Integer result = em.createQuery(query, Integer.class)
 //                    .getSingleR esult();
-            List<String> result = em.createQuery(query, String.class)
+
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .getResultList();
+
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
             
 //            for (Object o : result) {
 //                System.out.println("o = " + o);
 //            }
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+//            for (Member member : result) {
+//                // 페치 조인으로 회원과 팀을 함께 조회해서 지연 로딩 X
+//                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+//                // 회원1, 팀A(SQL)
+//                // 회원2, 팀A(1차캐시)
+//                // 회원3, 팀B(SQL)
+//
+//                // 만약 회원100명 -> N + 1 (+1인 이유는 첫번째로 날리는 쿼리에 대한 결과[비교를 위해])
+//            }
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "| members = " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
 
 //            System.out.println("result = " + result);
